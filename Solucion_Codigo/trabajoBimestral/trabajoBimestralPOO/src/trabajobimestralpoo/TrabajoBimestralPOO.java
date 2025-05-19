8cfe35ddebf1953e056700c8233da4768f912f1c
abrahamx02 <georssj@gmail.com>
2025-05-18 21:42:26 -0500

    Solucion java

---
+++ Solucion_Codigo/trabajoBimestral/trabajoBimestralPOO/src/trabajobimestralpoo/TrabajoBimestralPOO.java
@@ -0,0 +1,34 @@
+package trabajobimestralpoo;
+
+import java.util.Scanner;
+
+public class TrabajoBimestralPOO {
+
+    public static void main(String[] args) {
+        Scanner datos = new Scanner(System.in);
+        System.out.println("Los Horarios de que dia quiere observar?");
+        String op = datos.nextLine().toLowerCase();
+        switch (op) {
+            case "lunes":
+                cartelera.carteleraL();
+                break;
+            case "martes":
+                cartelera.carteleraM();
+                break;
+            case "miercoles":
+                cartelera.carteleraMC();
+                break;
+            case "jueves":
+                cartelera.carteleraJ();
+                break;
+            case "viernes":
+                cartelera.carteleraV();
+                break;
+            default:
+                System.out.println("Dia no valido ");
+                break;
+
+        }
+    }
+
+}
---
+++ Solucion_Codigo/trabajoBimestral/trabajoBimestralPOO/src/trabajobimestralpoo/cartelera.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajobimestralpoo;
+
+/**
+ *
+ * @author abrah
+ */
+public class cartelera {
+    public static void carteleraL(){
+
+        System.out.println("bien");
+}
+     public static void carteleraM(){
+          System.out.println("bien");
+         
+     }
+      public static void carteleraMC(){
+           System.out.println("bien");
+          
+      }
+ public static void carteleraJ(){
+      System.out.println("bien");
+     
+ }
+  public static void carteleraV(){
+       System.out.println("bien");
+      
+  }
+}
